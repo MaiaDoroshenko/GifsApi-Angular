09a31a25c639ab33c4096bc2a896583237315028
Maia Potskhverashvili <maiadoroshenko@gmail.com>
2023-03-28 12:32:27 -0300

    llamadas de Api con Http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
-
-  private apiKey: string = "imZxDadq7nkpSVHsFGqz14gRAAcrAnGD";
+  private apiKey: string = 'imZxDadq7nkpSVHsFGqz14gRAAcrAnGD';
 
   private _historial: string[] = [];
+
+  public resultados:any[]=[];
 
   get historial() {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
   buscarGifs(query: string) {
     query = query.trim().toLowerCase(); //borro loe espacios y lo paso todo a minuscula
@@ -26,6 +27,11 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10); //luego lo corto para que no sean mas de 10
     }
 
-    console.log(this._historial);
+    this.http.get(
+      `http://api.giphy.com/v1/gifs/search?api_key=imZxDadq7nkpSVHsFGqz14gRAAcrAnGD&q=${query}&limit=10`
+    ).subscribe((resp : any) => {
+      console.log(resp.data);
+      this.resultados=resp.data;
+    });
   }
 }
